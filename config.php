5d07f0569e9095b0dfadd02fa2cd4a1ef3783352
9bobo4 <slobotadic76@gmail.com>
2022-05-08 20:37:42 +0200

    napravljena login i register stranica

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+    $hostname = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "satovi";
+    
+    $conn = mysqli_connect($hostname,$username,$password,$database) or die("Database connection failed");
+
+    git config --global user.email "you@example.com"
+    git config --global user.name "Your Name"
+
+
+?>
